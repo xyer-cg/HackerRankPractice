dafdf4e6a05dd65a7bd4893bb2570ea3e3c75fad
XY <xyer24@outlook.com>
2018-12-02 16:36:55 +0800

    Initial commit

---
+++ CamelCase.java
@@ -0,0 +1,33 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CamelCase
+{
+    static int camelcase (String s)
+    {
+        int count = 1;
+        for (char c : s.toCharArray ()) if (Character.isUpperCase (c)) count ++;
+
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner (System.in);
+
+    public static void main (String[] args) throws IOException
+    {
+        BufferedWriter bufferedWriter = new BufferedWriter (new FileWriter (System.getenv ("OUTPUT_PATH")));
+
+        String s = scanner.nextLine ();
+
+        int result = camelcase (s);
+
+        bufferedWriter.write (String.valueOf (result));
+        bufferedWriter.newLine ();
+
+        bufferedWriter.close ();
+
+        scanner.close ();
+    }
+}
